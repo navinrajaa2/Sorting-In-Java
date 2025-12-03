bead505251b228ee9932ece44f5ec62d95b2871b
Navin Rajaa <navinrajaa02@gmail.com>
2025-12-03 10:58:53 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+
+public class bubblesort {
+  public static int[] bubblesor(int arr[]){
+    for (int i=0; i < arr.length; i++) {
+        for (int j=1;j<arr.length -i;j++){
+          if(arr[j]>arr[j-1]){
+            int temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+          }
+        }
+    }
+    return arr;
+  }
+  public static void main(String[] args) {
+      int[] arr ={9,8,7,6,5,4,3,2,1};
+      System.out.println(Arrays.toString(bubblesor(arr)));
+  }
+}
