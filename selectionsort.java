5d6b4c7cfeae0d4746008f7fa596915df9204dbf
Navin Rajaa <navinrajaa02@gmail.com>
2025-12-03 12:24:28 +0530

    Introduce user-friendly selection sort method
    Added a user-friendly selection sort method as a comment.

--- selectionsort.java
+++ selectionsort.java
@@ -27,5 +27,41 @@ public class selectionsort {
       }
       return max;
     }
-  
 }
+//THIS IS THE USE FRIENDLY METHOD
+/*import java.util.Arrays;
+
+public class SelectionSortEasy {
+
+    public static void main(String[] args) {
+        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        System.out.println("Before Sort: " + Arrays.toString(arr));
+
+        selectionSort(arr);
+
+        System.out.println("After Sort: " + Arrays.toString(arr));
+    }
+
+    public static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int minIndex = i;  // assume current index is smallest
+
+            // find the actual smallest element
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap smallest element with first unsorted element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+*/ 
+
