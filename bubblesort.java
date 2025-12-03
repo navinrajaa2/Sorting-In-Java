e3836787bafe7c757685c8718c8e0ad486fe8a8d
Navin Rajaa <navinrajaa02@gmail.com>
2025-12-03 11:48:08 +0530

    Optimize bubble sort with early termination
    Added a swapped flag to optimize the bubble sort algorithm by breaking early if the array is already sorted.

--- bubblesort.java
+++ bubblesort.java
@@ -1,17 +1,21 @@
-
 import java.util.Arrays;
-
 public class bubblesort {
   public static int[] bubblesor(int arr[]){
     for (int i=0; i < arr.length; i++) {
+      boolean swapped=false;
         for (int j=1;j<arr.length -i;j++){
-          if(arr[j]>arr[j-1]){//Here the viceversa it decending
+          if(arr[j]<arr[j-1]){
             int temp=arr[j];
             arr[j]=arr[j-1];
             arr[j-1]=temp;
+            swapped=true;
           }
         }
+        if(!swapped){// this is used to when the array this already sorted this break at the first iteration
+          break;
+        }
     }
+
     return arr;
   }
   public static void main(String[] args) {
@@ -19,4 +23,3 @@ public class bubblesort {
       System.out.println(Arrays.toString(bubblesor(arr)));
   }
 }
-
