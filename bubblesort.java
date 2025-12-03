7cc9c558c3cf5216b7719237da31932d56dc9f25
Navin Rajaa <navinrajaa02@gmail.com>
2025-12-03 11:05:39 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -5,7 +5,7 @@ public class bubblesort {
   public static int[] bubblesor(int arr[]){
     for (int i=0; i < arr.length; i++) {
         for (int j=1;j<arr.length -i;j++){
-          if(arr[j]>arr[j-1]){
+          if(arr[j]>arr[j-1]){//Here the viceversa it decending
             int temp=arr[j];
             arr[j]=arr[j-1];
             arr[j-1]=temp;
@@ -19,3 +19,4 @@ public class bubblesort {
       System.out.println(Arrays.toString(bubblesor(arr)));
   }
 }
+
